99a57e1a853096c2d26d37ea330c4e5f2d43a699
Gilmar <gilmardealcantara@gmail.com>
2020-11-17 01:00:48 -0300

    config form for validate fields

--- src/containers/RequestPage/AddNewExpense/NewExpenseForm/index.tsx
+++ src/containers/RequestPage/AddNewExpense/NewExpenseForm/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Alert, DatePicker, Input, InputNumber, notification, Select } from 'antd';
+import { Alert, Button, DatePicker, Form, Input, InputNumber, notification, Select } from 'antd';
 import { SmileOutlined } from '@ant-design/icons';
 
 import { expenseTypesOptions, currencyOptions, currencyFormatter, currencyParser } from './FormHelper';
@@ -11,11 +11,10 @@ import FetchAPI from '../../../../services/FetchApi';
 
 interface Expense {
     expenseTypeCode?: string;
-    currencyCode?: string
-    currencyCodeKey?: string
-    amountSpent?: number,
-    amountTotal?: number,
-    notes?: string,
+    currencyCode?: string;
+    amountSpent?: number;
+    amountTotal?: number;
+    notes?: string;
     resourceUrl?: string;
     cardDate?: number;
 }
@@ -26,11 +25,10 @@ interface ApiResponse {
 }
 
 const NewExpenseForm = () => {
-    const [expense, setExpense] = useState<Expense>({});
     const [response, setResponse] = useState<ApiResponse>();
     const [loading, setLoading] = useState<boolean>();
-
-    const amountParser = (value: number | string | undefined) => Number((value as number).toFixed(2))
+    const [currencyCodeKey, setCurrencyCodeKey] = useState<string>();
+    const [form] = Form.useForm();
 
     const openNotification = (response: ApiResponse) => {
         notification.open({
@@ -39,100 +37,114 @@ const NewExpenseForm = () => {
         });
     };
 
-    const addNewExpense = async () => {
+    const resetForm = () => {
+        form.resetFields();
+        setCurrencyCodeKey(undefined);
+    }
+
+    const addNewExpense = async (expense: Expense) => {
+
         const fetchApi = new FetchAPI(API_URL);
         setLoading(true);
-        const { data, error } = await fetchApi.post("/expense/ad", expense);
+        const { data, error } = await fetchApi.post("/expense/add", expense);
         setLoading(false);
         if (!error) {
-            setExpense({});
             openNotification({ message: "Despesa adicionda com sucesso", type: "success" })
+            resetForm();
         } else {
             openNotification({ message: "Falha ao salvar dados, tente novamente mais tarde", type: "error" })
         }
     }
 
-    console.log(expense)
+
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+        const payload = {
+            expenseTypeCode: values.expenseTypeCode,
+            currencyCode: values.currencyCodeKey.split("::")[1],
+            amountSpent: Number((values.amountSpent).toFixed(2)),
+            amountTotal: Number((values.amountTotal).toFixed(2)),
+            notes: values.notes,
+            resourceUrl: values.resourceUrl,
+            cardDate: values.cardDate.valueOf(),
+        }
+        addNewExpense(payload);
+    };
+
 
     return (
         <div className="receipt-form-container">
-            <div className="receipt-form-fields">
-                <div className="receipt-form-field">
-                    <span >Tipo *</span>
-                    <Select
-                        placeholder="Tipo"
-                        onChange={(value: string) => setExpense({ ...expense, expenseTypeCode: value })}
-                    >
-                        {expenseTypesOptions.map(op => <Select.Option key={op.value} value={op.value}>{op.label}</Select.Option>)}
-                    </Select>
-                </div>
-                <div className="receipt-form-field">
-                    <span >Moeda *</span>
-                    <Select
-                        placeholder="Moeda"
-                        onChange={(value: string) => {
-                            setExpense({ ...expense, currencyCode: value.split("::")[1], currencyCodeKey: value })
-                        }}
-                    >
-                        {currencyOptions.map(op => <Select.Option key={op.value} value={op.value}>{op.label}</Select.Option>)}
-                    </Select>
-                </div>
-            </div>
-            <div className="receipt-form-field">
-                <span >Descrição da despesa</span>
-                <Input
-                    onChange={e => setExpense({ ...expense, notes: e.target.value })}
-                    style={{ width: '48%' }}
-                    placeholder="Descrição da despesa"
-                />
-            </div>
-            <div className="receipt-form-field">
-                <span >Data do comprovante</span>
-                <DatePicker
-                    placeholder="Selecione uma data"
-                    style={{ width: '48%' }}
-                    format={"DD/MM/YYYY"}
-                    onChange={(date) => setExpense({ ...expense, cardDate: date?.valueOf() })}
-                />
-            </div>
-            {expense.currencyCodeKey && (
+            <Form
+                form={form}
+                onFinish={onFinish}
+            >
                 <div className="receipt-form-fields">
                     <div className="receipt-form-field">
-                        <span >Valor total da nota/cupom</span>
-                        <InputNumber
-                            defaultValue={0}
-                            style={{
-                                width: '100%',
-                                marginRight: "1rem"
-                            }}
-                            formatter={currencyFormatter(expense.currencyCodeKey)}
-                            parser={currencyParser}
-                            onChange={(value) => setExpense({ ...expense, amountTotal: amountParser(value) })}
-                        />
+                        <span >Tipo *</span>
+                        <Form.Item name="expenseTypeCode" rules={[{ required: true, message: 'Obrigatório' }]} >
+                            <Select placeholder="Tipo" >
+                                {expenseTypesOptions.map(op => <Select.Option key={op.value} value={op.value}>{op.label}</Select.Option>)}
+                            </Select>
+                        </Form.Item>
                     </div>
                     <div className="receipt-form-field">
-                        <span >Valor a ser considerado</span>
-                        <InputNumber
-                            defaultValue={0}
-                            style={{
-                                width: '100%',
-                                marginRight: "1rem"
-                            }}
-                            formatter={currencyFormatter(currencyOptions[0].value)}
-                            parser={currencyParser}
-                            onChange={(value) => setExpense({ ...expense, amountTotal: amountParser(value) })}
-                        />
+                        <span >Moeda *</span>
+                        <Form.Item name="currencyCodeKey" rules={[{ required: true, message: 'Obrigatório' }]} >
+                            <Select placeholder="Moeda" onChange={(value: string) => { setCurrencyCodeKey(value) }} >
+                                {currencyOptions.map(op => <Select.Option key={op.value} value={op.value}>{op.label}</Select.Option>)}
+                            </Select>
+                        </Form.Item>
                     </div>
                 </div>
-            )}
-            {response && <Alert message={response.message} type={response.type} closable afterClose={() => setResponse(undefined)} />}
-            <div className="action-buttons">
-                <span>Cancelar</span>
-                <span onClick={addNewExpense}>
-                    Salvar
-                   <i className="fas fa-chevron-down"></i>
-                </span>
-            </div>
+                <div className="receipt-form-field">
+                    <span >Descrição da despesa</span>
+                    <Form.Item name="notes" rules={[{ required: true, message: 'Obrigatório' }]}  >
+                        <Input style={{ width: '48%' }} placeholder="Descrição da despesa" />
+                    </Form.Item>
+                </div>
+                <div className="receipt-form-field">
+                    <span >Data do comprovante</span>
+                    <Form.Item name="cardDate" rules={[{ required: true, message: 'Obrigatório' }]}  >
+                        <DatePicker placeholder="Selecione uma data" style={{ width: '48%' }} format={"DD/MM/YYYY"} />
+                    </Form.Item>
+                </div>
+                {currencyCodeKey && (
+                    <div className="receipt-form-fields">
+                        <div className="receipt-form-field">
+                            <span >Valor total da nota/cupom</span>
+                            <Form.Item name="amountTotal" rules={[{ required: true, message: 'Deve conter um valor acima de zero' }]}  >
+                                <InputNumber
+                                    defaultValue={0}
+                                    style={{
+                                        width: '100%',
+                                        marginRight: "1rem"
+                                    }}
+                                    formatter={currencyFormatter(currencyCodeKey)}
+                                    parser={currencyParser}
+                                />
+                            </Form.Item>
+                        </div>
+                        <div className="receipt-form-field">
+                            <span >Valor a ser considerado</span>
+                            <Form.Item name="amountSpent" rules={[{ required: true, message: 'Deve conter um valor acima de zero' }]}  >
+                                <InputNumber
+                                    defaultValue={0}
+                                    style={{
+                                        width: '100%',
+                                        marginRight: "1rem"
+                                    }}
+                                    formatter={currencyFormatter(currencyCodeKey)}
+                                    parser={currencyParser}
+                                />
+                            </Form.Item>
+                        </div>
+                    </div>
+                )}
+                <div className="action-buttons">
+                    <Button onClick={() => resetForm()} > Cancelar </Button>
+                    <Button type="primary" htmlType="submit"> Salvar </Button>
+                </div>
+            </Form>
         </div>
     );
 }
