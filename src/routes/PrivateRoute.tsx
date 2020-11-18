b4ea683654c00a96ddb1b56de9661afb03994250
Gilmar <gilmardealcantara@gmail.com>
2020-11-18 02:16:31 -0300

    use eslint from react scripts

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import {
-    Route,
-    Redirect,
-    RouteProps,
-    RouteComponentProps,
+  Route,
+  Redirect,
+  RouteProps,
+  RouteComponentProps,
 } from 'react-router-dom';
 import { TOKEN_STORAGE_KEY } from '../utils/constants';
 import BasePage from '../containers/Base';
 
-export const isAuthenticated = () =>
-    localStorage.getItem(TOKEN_STORAGE_KEY) === null;
+export const isAuthenticated = (): boolean =>
+  localStorage.getItem(TOKEN_STORAGE_KEY) === null;
 
 const PrivateRoute = ({ component, ...rest }: RouteProps) => {
-    if (!component) {
-        throw Error('component is undefined');
-    }
+  if (!component) {
+    throw Error('component is undefined');
+  }
 
-    if (isAuthenticated()) {
-        const render = (props: RouteComponentProps): React.ReactNode =>
-            <BasePage component={component} routeProps={props} />;
+  if (isAuthenticated()) {
+    const render = (props: RouteComponentProps): React.ReactNode => (
+      <BasePage component={component} routeProps={props} />
+    );
 
-        return <Route {...rest} render={render} />;
-    }
-    return <Redirect to={{ pathname: '/login' }} />;
+    return <Route {...rest} render={render} />;
+  }
+  return <Redirect to={{ pathname: '/login' }} />;
 };
 
 export default PrivateRoute;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 
-import history from './history';
-import PrivateRoute from '../routes/PrivateRoute';
-import RequestPage from '../containers/RequestPage';
-import Home from '../containers/Home';
+import history from 'src/routes/history';
+import PrivateRoute from 'src/routes/PrivateRoute';
+import RequestPage from 'src/containers/RequestPage';
+import Home from 'src/containers/Home';
 
+const Routes: React.FC = () => (
+  <ConnectedRouter history={history}>
+    <Switch>
+      <Route path='/login' component={() => <div>Login</div>} />
+      <PrivateRoute path='/requests' component={RequestPage} />
+      <PrivateRoute path='/' component={Home} />
+    </Switch>
+  </ConnectedRouter>
+);
 
-export default function Routes() {
-    return (
-        <ConnectedRouter history={history}>
-            <Switch>
-                <Route path="/login" component={() => <div>Login</div>} />
-                <PrivateRoute path="/requests" component={RequestPage} />
-                <PrivateRoute path="/" component={Home} />
-            </Switch>
-        </ConnectedRouter>
-    );
-}
+export default Routes;
